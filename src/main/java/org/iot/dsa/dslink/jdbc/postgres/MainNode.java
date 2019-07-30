85d093f97dc9aa06d49556fb15183ecbce666450
Aaron <a-hansen@users.noreply.github.com>
2019-07-30 09:57:19 -0700

    0.0.5
    - Updates for core jdbc library changes.

--- src/main/java/org/iot/dsa/dslink/jdbc/postgres/MainNode.java
+++ src/main/java/org/iot/dsa/dslink/jdbc/postgres/MainNode.java
@@ -2,7 +2,6 @@ package org.iot.dsa.dslink.jdbc.postgres;
 
 import org.iot.dsa.dslink.jdbc.AbstractMainNode;
 import org.iot.dsa.dslink.jdbc.JDBCPooledNode;
-import org.iot.dsa.dslink.jdbc.JDBCv2Helpers;
 import org.iot.dsa.node.DSElement;
 import org.iot.dsa.node.DSInfo;
 import org.iot.dsa.node.DSMap;
@@ -13,32 +12,32 @@ import org.iot.dsa.node.action.ActionResult;
 import org.iot.dsa.node.action.DSAction;
 
 public class MainNode extends AbstractMainNode {
-	
-	@Override
-	protected DSAction makeAddDatabaseAction() {
-		DSAction act = new DSAction.Parameterless() {
+
+    @Override
+    protected String getHelpUrl() {
+        return "https://github.com/iot-dsa-v2/dslink-java-v2-jdbc-postgres";
+    }
+
+    @Override
+    protected DSAction makeNewConnectionAction() {
+        DSAction act = new DSAction.Parameterless() {
             @Override
             public ActionResult invoke(DSInfo target, ActionInvocation invocation) {
                 return ((MainNode) target.get()).addNewDatabase(invocation.getParameters());
             }
         };
-        act.addParameter(JDBCv2Helpers.DB_NAME, DSValueType.STRING, null);
-        act.addParameter(JDBCv2Helpers.DB_URL, DSValueType.STRING, null)
+        act.addParameter(DB_NAME, DSValueType.STRING, null);
+        act.addParameter(DB_URL, DSValueType.STRING, null)
            .setPlaceHolder("jdbc:postgresql://127.0.0.1:3306");
-        act.addParameter(JDBCv2Helpers.DB_USER, DSValueType.STRING, null);
-        act.addParameter(JDBCv2Helpers.DB_PASSWORD, DSValueType.STRING, null).setEditor("password");
+        act.addParameter(DB_USER, DSValueType.STRING, null);
+        act.addParameter(DB_PASSWORD, DSValueType.STRING, null).setEditor("password");
         return act;
-	}
-	
-	@Override
-	protected String getHelpUrl() {
-		return "https://github.com/iot-dsa-v2/dslink-java-v2-jdbc-postgres";
-	}
-	
-	private ActionResult addNewDatabase(DSMap parameters) {
-		parameters.put(JDBCv2Helpers.DRIVER, DSElement.make("org.postgresql.Driver"));
+    }
+
+    private ActionResult addNewDatabase(DSMap parameters) {
+        parameters.put(DRIVER, DSElement.make("org.postgresql.Driver"));
         DSNode nextDB = new JDBCPooledNode(parameters);
-        add(parameters.getString(JDBCv2Helpers.DB_NAME), nextDB);
+        add(parameters.getString(DB_NAME), nextDB);
         return null;
     }
 }
